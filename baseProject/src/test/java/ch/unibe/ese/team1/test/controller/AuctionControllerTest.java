ea57002d133769ae4187344517e61a391d0f9c5c
Julian Schuhmacher <julian.schuhmacher@students.unibe.ch>
2016-11-11 21:29:30 +0100

    Add test for two controller and for the AuctionSchedule

---
+++ baseProject/src/test/java/ch/unibe/ese/team1/test/controller/AuctionControllerTest.java
@@ -0,0 +1,48 @@
+package ch.unibe.ese.team1.test.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import ch.unibe.ese.team1.controller.pojos.forms.MessageForm;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/config/springMVC.xml",
+		"file:src/main/webapp/WEB-INF/config/springData.xml",
+		"file:src/main/webapp/WEB-INF/config/springSecurity.xml" })
+public class AuctionControllerTest {
+
+	@Autowired
+	WebApplicationContext wac;
+	@Autowired
+	MockHttpServletRequest request;
+
+	private MockMvc mockMvc;
+
+	@Before
+	public void setup() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+	}
+	
+	@Test
+	public void getAuction() throws Exception {
+		this.mockMvc.perform(get("/auction").param("id", "1"))
+			.andExpect(status().isOk())
+			.andExpect(model().attributeExists("shownAuction", "messageForm", "loggedInUserEmail", "visits"))
+			.andExpect(view().name("auctionDescription"));
+	}
+}
