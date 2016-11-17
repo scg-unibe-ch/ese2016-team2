ff6079a488e05a30eaa125c55e3b043dde3ef923
Julian Schuhmacher <julian.schuhmacher@students.unibe.ch>
2016-11-17 17:02:38 +0100

    Delete controller for '~' - files

--- baseProject/src/main/java/ch/unibe/ese/team1/controller/AdController.java
+++ baseProject/src/main/java/ch/unibe/ese/team1/controller/AdController.java
@@ -1,28 +1,22 @@
 package ch.unibe.ese.team1.controller;
 
 import java.security.Principal;
-import java.util.List;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import ch.unibe.ese.team1.controller.pojos.forms.MessageForm;
 import ch.unibe.ese.team1.controller.service.AdService;
-import ch.unibe.ese.team1.controller.service.BookmarkService;
 import ch.unibe.ese.team1.controller.service.MessageService;
-import ch.unibe.ese.team1.controller.service.UserService;
 import ch.unibe.ese.team1.controller.service.VisitService;
 import ch.unibe.ese.team1.model.Ad;
-import ch.unibe.ese.team1.model.User;
 
 
 /**
@@ -43,12 +37,6 @@ public class AdController {
 
 	@Autowired
 	private AdService adService;
-
-	@Autowired
-	private UserService userService;
-
-	@Autowired
-	private BookmarkService bookmarkService;
 
 	@Autowired
 	private MessageService messageService;
@@ -82,26 +70,6 @@ public class AdController {
 		return model;
 	}
 
-	//@Jerome
-	//Tile-Sibling
-	@RequestMapping(value = "/~ad", method = RequestMethod.GET)
-	public ModelAndView _ad(@RequestParam("id") long id, Principal principal) {
-		ModelAndView model = new ModelAndView("~adDescription");
-		Ad ad = adService.getAdById(id);
-		model.addObject("shownAd", ad);
-		model.addObject("messageForm", new MessageForm());
-
-		String loggedInUserEmail = (principal == null) ? "" : principal
-				.getName();
-		model.addObject("loggedInUserEmail", loggedInUserEmail);
-
-		model.addObject("visits", visitService.getVisitsByAd(ad));
-
-		return model;
-	}
-
-
-
 	/**
 	 * Gets the ad description page for the ad with the given id and also
 	 * validates and persists the message passed as post data.
@@ -120,24 +88,6 @@ public class AdController {
 		}
 		return model;
 	}
-
-	//@Jerome
-	//Tile-Sibling
-	@RequestMapping(value = "/~ad", method = RequestMethod.POST)
-	public ModelAndView _messageSent(@RequestParam("id") long id,
-			@Valid MessageForm messageForm, BindingResult bindingResult) {
-
-		ModelAndView model = new ModelAndView("~adDescription");
-		Ad ad = adService.getAdById(id);
-		model.addObject("shownAd", ad);
-		model.addObject("messageForm", new MessageForm());
-
-		if (!bindingResult.hasErrors()) {
-			messageService.saveFrom(messageForm);
-		}
-		return model;
-	}
-
 
 	/**
 	 * @Jerome
