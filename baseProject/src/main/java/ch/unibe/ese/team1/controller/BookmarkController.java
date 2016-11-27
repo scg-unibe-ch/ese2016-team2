7f5c494380b133fcaa1a72c13f4c8db8dcde2244
Julian Schuhmacher <julian.schuhmacher@students.unibe.ch>
2016-11-27 17:28:02 +0100

    Add BookmarkController and link to bookmark auctions. Add new list for a user which holds the bookmarked auctions.

--- baseProject/src/main/java/ch/unibe/ese/team1/controller/BookmarkController.java
+++ baseProject/src/main/java/ch/unibe/ese/team1/controller/BookmarkController.java
@@ -16,10 +16,11 @@ import ch.unibe.ese.team1.controller.service.AuctionService;
 import ch.unibe.ese.team1.controller.service.BookmarkService;
 import ch.unibe.ese.team1.controller.service.UserService;
 import ch.unibe.ese.team1.model.Ad;
+import ch.unibe.ese.team1.model.Auction;
 import ch.unibe.ese.team1.model.User;
 
 @Controller
-public class AdvertisementController {
+public class BookmarkController {
 	
 	@Autowired
 	UserService userService;
@@ -67,6 +68,52 @@ public class AdvertisementController {
 				}
 			}
 			for (Ad adIterable : bookmarkedAdsIterable) {
+				if (adIterable.getId() == id) {
+					return 3;
+				}
+			}
+			return 2;
+		}
+
+		Ad ad = adService.getAdById(id);
+
+		return bookmarkService.getBookmarkStatus(ad, bookmarked, user);
+	}
+	
+	/**
+	 * Checks if the auctionID passed as post parameter is already inside user's
+	 * List bookmarkedAuctions. In case it is present, true is returned changing
+	 * the "Bookmark Auction" button to "Bookmarked". If it is not present it is
+	 * added to the List bookmarkedAuctions.
+	 *
+	 * @return 0 and 1 for errors; 3 to update the button to bookmarked 3 and 2
+	 *         for bookmarking or undo bookmarking respectively 4 for removing
+	 *         button completly (because its the users ad)
+	 */
+	@RequestMapping(value = "/bookmarkAuction", method = RequestMethod.POST)
+	@Transactional
+	@ResponseBody
+	public int isBookmarkedAuction(@RequestParam("id") long id,
+			@RequestParam("screening") boolean screening,
+			@RequestParam("bookmarked") boolean bookmarked, Principal principal) {
+		// should never happen since no bookmark button when not logged in
+		if (principal == null) {
+			return 0;
+		}
+		String username = principal.getName();
+		User user = userService.findUserByUsername(username);
+		if (user == null) {
+			// that should not happen...
+			return 1;
+		}
+		List<Auction> bookmarkedAuctionsIterable = user.getBookmarkedAuctions();
+		if (screening) {
+			for (Auction ownAdIterable : auctionService.getAuctionsByUser(user)) {
+				if (ownAdIterable.getId() == id) {
+					return 4;
+				}
+			}
+			for (Auction adIterable : bookmarkedAuctionsIterable) {
 				if (adIterable.getId() == id) {
 					return 3;
 				}
