d49bb63120c3bb43b1cf61bd9700bb93ab2c4726
J S <jerome@shtill.com>
2016-11-29 23:26:50 +0100

    Remove auto scroll on tab to input.

--- baseProject/src/main/webapp/resources/js/prod/placeAd.js
+++ baseProject/src/main/webapp/resources/js/prod/placeAd.js
@@ -29496,16 +29496,16 @@ jQuery.flatfindr.register({
     /**
      * All input fields within the search form scrollable container.
      */
-    $('.form-search .container-scroll input')
-      .focus(alignInputToTop);
+    // $('.form-search .container-scroll input')
+    //   .focus(alignInputToTop);
 
 
 
     /**
      * All checkboxes within the search form scrollable container.
      */
-    $('.form-search .container-scroll label')
-      .on('click', alignInputToTop);
+    // $('.form-search .container-scroll label')
+    //   .on('click', alignInputToTop);
 
 
 
